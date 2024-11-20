b9lab/my-collection-manager

Add cross-contract query.

src/contract.rs
@@ -1,10 +1,13 @@
 use crate::{
     error::ContractError,
-    msg::{CollectionExecuteMsg, ExecuteMsg, InstantiateMsg},
+    msg::{CollectionExecuteMsg, CollectionQueryMsg, ExecuteMsg, InstantiateMsg},
 };
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_json_binary, DepsMut, Env, MessageInfo, Response, WasmMsg};
+use cosmwasm_std::{
+    to_json_binary, DepsMut, Env, Event, MessageInfo, QueryRequest, Response, WasmMsg, WasmQuery,
+};
+use cw721::msg::NumTokensResponse;
 
 type ContractResult = Result<Response, ContractError>;
 
@@ -24,7 +27,7 @@ pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> C
 }
 
 fn execute_pass_through(
-    _: DepsMut,
+    deps: DepsMut,
     _: Env,
     info: MessageInfo,
     collection: String,
@@ -35,18 +38,92 @@ fn execute_pass_through(
         msg: to_json_binary(&message)?,
         funds: info.funds,
     };
-    Ok(Response::default().add_message(onward_exec_msg))
+    let token_count_result =
+        deps.querier
+            .query::<NumTokensResponse>(&QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: collection,
+                msg: to_json_binary(&CollectionQueryMsg::NumTokens {})?,
+            }));
+    let token_count_event = Event::new("my-collection-manager")
+        .add_attribute("token-count-before", token_count_result?.count.to_string());
+    Ok(Response::default()
+        .add_message(onward_exec_msg)
+        .add_event(token_count_event))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::msg::{CollectionExecuteMsg, ExecuteMsg};
-    use cosmwasm_std::{testing, to_json_binary, Addr, Coin, Response, Uint128, WasmMsg};
+    use crate::msg::{CollectionExecuteMsg, CollectionQueryMsg, ExecuteMsg};
+    use cosmwasm_std::{
+        from_json,
+        testing::{self, MockApi, MockQuerier, MockStorage},
+        to_json_binary, Addr, Coin, ContractResult, Empty, Event, OwnedDeps, Querier,
+        QuerierResult, QueryRequest, Response, SystemError, SystemResult, Uint128, WasmMsg,
+        WasmQuery,
+    };
+    use cw721::msg::NumTokensResponse;
+    use std::marker::PhantomData;
+
+    pub fn mock_deps(
+        response: NumTokensResponse,
+    ) -> OwnedDeps<MockStorage, MockApi, NumTokensMockQuerier, Empty> {
+        OwnedDeps {
+            storage: MockStorage::default(),
+            api: MockApi::default(),
+            querier: NumTokensMockQuerier::new(MockQuerier::new(&[]), response),
+            custom_query_type: PhantomData,
+        }
+    }
+
+    pub struct NumTokensMockQuerier {
+        base: MockQuerier,
+        response: NumTokensResponse,
+    }
+
+    impl Querier for NumTokensMockQuerier {
+        fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+            let request: QueryRequest<Empty> = match from_json(bin_request) {
+                Ok(v) => v,
+                Err(e) => {
+                    return SystemResult::Err(SystemError::InvalidRequest {
+                        error: format!("Parsing query request: {}", e),
+                        request: bin_request.into(),
+                    })
+                }
+            };
+
+            self.handle_query(&request)
+        }
+    }
+
+    impl NumTokensMockQuerier {
+        pub fn handle_query(&self, request: &QueryRequest<Empty>) -> QuerierResult {
+            match &request {
+                QueryRequest::Wasm(WasmQuery::Smart {
+                    contract_addr: _,
+                    msg,
+                }) => {
+                    let expected = to_json_binary(&CollectionQueryMsg::NumTokens {})
+                        .expect("Failed to create expected query");
+                    assert_eq!(expected.to_vec(), msg.to_vec(), "Query is not num tokens");
+                    SystemResult::Ok(ContractResult::Ok(
+                        to_json_binary(&self.response)
+                            .expect("Failed to serialize num tokens response"),
+                    ))
+                }
+                _ => self.base.handle_query(request),
+            }
+        }
+
+        pub fn new(base: MockQuerier<Empty>, response: NumTokensResponse) -> Self {
+            NumTokensMockQuerier { base, response }
+        }
+    }
 
     #[test]
     fn test_pass_through() {
         // Arrange
-        let mut mocked_deps_mut = testing::mock_dependencies();
+        let mut mocked_deps_mut = mock_deps(NumTokensResponse { count: 3 });
         let mocked_env = testing::mock_env();
         let executer = Addr::unchecked("executer");
         let fund_sent = Coin {
@@ -78,11 +155,15 @@ mod tests {
         // Assert
         assert!(contract_result.is_ok(), "Failed to pass message through");
         let received_response = contract_result.unwrap();
-        let expected_response = Response::default().add_message(WasmMsg::Execute {
-            contract_addr: "collection".to_owned(),
-            msg: to_json_binary(&inner_msg).expect("Failed to serialize inner message"),
-            funds: vec![fund_sent],
-        });
+        let expected_response = Response::default()
+            .add_message(WasmMsg::Execute {
+                contract_addr: "collection".to_owned(),
+                msg: to_json_binary(&inner_msg).expect("Failed to serialize inner message"),
+                funds: vec![fund_sent],
+            })
+            .add_event(
+                Event::new("my-collection-manager").add_attribute("token-count-before", "3"),
+            );
         assert_eq!(received_response, expected_response);
     }
 }

b9lab/cw-my-collection-manager

src/contract.rs
@@ -1,10 +1,13 @@
 use crate::{
     error::ContractError,
-    msg::{CollectionExecuteMsg, ExecuteMsg, InstantiateMsg},
+    msg::{CollectionExecuteMsg, CollectionQueryMsg, ExecuteMsg, InstantiateMsg},
 };
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_json_binary, DepsMut, Env, MessageInfo, Response, WasmMsg};
+use cosmwasm_std::{
+    to_json_binary, DepsMut, Env, Event, MessageInfo, QueryRequest, Response, WasmMsg, WasmQuery,
+};
+use cw721::msg::NumTokensResponse;
 
 type ContractResult = Result<Response, ContractError>;
 
@@ -24,7 +27,7 @@ pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> C
 }
 
 fn execute_pass_through(
-    _: DepsMut,
+    deps: DepsMut,
     _: Env,
     info: MessageInfo,
     collection: String,
@@ -35,18 +38,92 @@ fn execute_pass_through(
         msg: to_json_binary(&message)?,
         funds: info.funds,
     };
-    Ok(Response::default().add_message(onward_exec_msg))
+    let token_count_result =
+        deps.querier
+            .query::<NumTokensResponse>(&QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: collection,
+                msg: to_json_binary(&CollectionQueryMsg::NumTokens {})?,
+            }));
+    let token_count_event = Event::new("my-collection-manager")
+        .add_attribute("token-count-before", token_count_result?.count.to_string());
+    Ok(Response::default()
+        .add_message(onward_exec_msg)
+        .add_event(token_count_event))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::msg::{CollectionExecuteMsg, ExecuteMsg};
-    use cosmwasm_std::{testing, to_json_binary, Addr, Coin, Response, Uint128, WasmMsg};
+    use crate::msg::{CollectionExecuteMsg, CollectionQueryMsg, ExecuteMsg};
+    use cosmwasm_std::{
+        from_json,
+        testing::{self, MockApi, MockQuerier, MockStorage},
+        to_json_binary, Addr, Coin, ContractResult, Empty, Event, OwnedDeps, Querier,
+        QuerierResult, QueryRequest, Response, SystemError, SystemResult, Uint128, WasmMsg,
+        WasmQuery,
+    };
+    use cw721::msg::NumTokensResponse;
+    use std::marker::PhantomData;
+
+    pub fn mock_deps(
+        response: NumTokensResponse,
+    ) -> OwnedDeps<MockStorage, MockApi, NumTokensMockQuerier, Empty> {
+        OwnedDeps {
+            storage: MockStorage::default(),
+            api: MockApi::default(),
+            querier: NumTokensMockQuerier::new(MockQuerier::new(&[]), response),
+            custom_query_type: PhantomData,
+        }
+    }
+
+    pub struct NumTokensMockQuerier {
+        base: MockQuerier,
+        response: NumTokensResponse,
+    }
+
+    impl Querier for NumTokensMockQuerier {
+        fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+            match from_json::<QueryRequest<Empty>>(bin_request) {
+                Ok(request) => self.handle_query(&request),
+                Err(e) => SystemResult::Err(SystemError::InvalidRequest {
+                    error: format!("Parsing query request: {}", e),
+                    request: bin_request.into(),
+                }),
+            }
+        }
+    }
+
+    impl NumTokensMockQuerier {
+        pub fn handle_query(&self, request: &QueryRequest<Empty>) -> QuerierResult {
+            match request {
+                QueryRequest::Wasm(wasm_query) => match wasm_query {
+                    WasmQuery::Smart {
+                        contract_addr: _,
+                        msg,
+                    } => {
+                        let serialized = from_json::<CollectionQueryMsg>(msg)
+                            .map(|collection_query| match collection_query {
+                                CollectionQueryMsg::NumTokens {} => to_json_binary(&self.response)
+                                    .expect("Failed to serialize num tokens response"),
+                                _ => unimplemented!("{:?}", collection_query),
+                            })
+                            .expect("Failed to find serialised type");
+                        SystemResult::Ok(ContractResult::Ok(serialized))
+                    }
+                    _ => unimplemented!("{:?}", wasm_query),
+                },
+                _ => self.base.handle_query(request),
+            }
+        }
+
+        pub fn new(base: MockQuerier<Empty>, response: NumTokensResponse) -> Self {
+            NumTokensMockQuerier { base, response }
+        }
+    }
 
     #[test]
     fn test_pass_through() {
         // Arrange
-        let mut mocked_deps_mut = testing::mock_dependencies();
+        let mut mocked_deps_mut = mock_deps(NumTokensResponse { count: 3 });
         let mocked_env = testing::mock_env();
         let executer = Addr::unchecked("executer");
         let fund_sent = Coin {
@@ -78,11 +155,15 @@ mod tests {
         // Assert
         assert!(contract_result.is_ok(), "Failed to pass message through");
         let received_response = contract_result.unwrap();
-        let expected_response = Response::default().add_message(WasmMsg::Execute {
-            contract_addr: "collection".to_owned(),
-            msg: to_json_binary(&inner_msg).expect("Failed to serialize inner message"),
-            funds: vec![fund_sent],
-        });
+        let expected_response = Response::default()
+            .add_message(WasmMsg::Execute {
+                contract_addr: "collection".to_owned(),
+                msg: to_json_binary(&inner_msg).expect("Failed to serialize inner message"),
+                funds: vec![fund_sent],
+            })
+            .add_event(
+                Event::new("my-collection-manager").add_attribute("token-count-before", "3"),
+            );
         assert_eq!(received_response, expected_response);
     }
 }